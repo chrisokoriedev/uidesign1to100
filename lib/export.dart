e4d420dcba0b178bc74abf47a3a584b3bde9bc2e
chrisokoriedev <okoriec01@gmail.com>
2025-08-01 08:22:55 +0100

    update

--- lib/export.dart
+++ lib/export.dart
@@ -5,6 +5,9 @@ export 'package:uidesign1to100/const/appcolor.dart';
 export 'package:uidesign1to100/widget/animated_text.dart';
 export 'package:animate_do/animate_do.dart';
 export 'package:uidesign1to100/pages/main_screen/main_list.dart';
+export 'package:uidesign1to100/pages/day1/dayone.dart';
+export 'package:uidesign1to100/pages/day2/demo.dart';
+export 'package:uidesign1to100/pages/day3/day3.dart';
 
 // export '';
 // export '';
--- lib/pages/main_screen/main_list.dart
+++ lib/pages/main_screen/main_list.dart
@@ -1,10 +1,36 @@
 import 'package:uidesign1to100/export.dart';
 
+List<Widget> screen = [DayOne(), Demo(), Demo3()];
+
 class MainListScreen extends StatelessWidget {
   const MainListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAnimatedList(
+          initialItemCount: screen.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+            Animation<double> animation,
+          ) {
+            return SlideTransition(
+              position: animation.drive(
+                Tween<Offset>(
+                  begin: const Offset(1, 0),
+                  end: Offset.zero,
+                ).chain(CurveTween(curve: Curves.easeInOut)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: screen[index],
+              ),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
