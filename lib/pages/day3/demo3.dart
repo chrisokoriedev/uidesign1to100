8849000059aaa7e12a4f755ce49893a40cdc8a4c
chrisokoriedev <okoriec01@gmail.com>
2025-08-10 06:47:01 +0100

    feat: Implement Demo3 widget with UI elements and update pubspec.yaml for new dependencies

--- lib/pages/day3/demo3.dart
+++ lib/pages/day3/demo3.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:uidesign1to100/util/export.dart';
 
 class Demo3 extends StatelessWidget {
   const Demo3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Chrisbites',
+              style: GoogleFonts.poppins(
+                fontSize: 30.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
